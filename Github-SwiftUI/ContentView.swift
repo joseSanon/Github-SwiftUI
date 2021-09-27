46bd5c13d4cfc15604877d96b388f62cdaeab237
Jose M. SANON <jose@MacBook-Pro-de-Alex.local>
2021-09-27 20:41:51 +0200

    Initial commit

---
+++ Github-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github-SwiftUI
+//
+//  Created by Jose M. SANON on 27/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Ola !")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Github-SwiftUI/Github_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_SwiftUIApp.swift
+//  Github-SwiftUI
+//
+//  Created by Jose M. SANON on 27/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Github_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
